8b66202b8e51bd5fe27f7833ebc363e9edb029c5
Frankyston Lins <frankyston@gmail.com>
2020-12-27 12:19:46 -0300

    Adding CRUD of Users

---
+++ app/controllers/admin/v1/users_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Admin::V1
+  class UsersController < ApiController
+    before_action :set_user, only: [:update, :destroy]
+
+    def index
+      @users = User.all
+    end
+
+    def create
+      @user = User.new
+      @user.attributes = user_params
+      save_user!
+    end
+
+    def update
+      @user.attributes = user_params
+      save_user!
+    end
+
+    def destroy
+      @user.destroy!
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      return {} unless params.has_key?(:user)
+
+      params.require(:user).permit(:id, :name, :email, :profile, :password, :password_confirmation)
+    end
+
+    def save_user!
+      @user.save!
+      render :show
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
+  end
+end
---
+++ spec/requests/admin/v1/users/client_requests_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Admin::V1::Users as :client', type: :request do
+  let(:login_user) { create(:user, profile: :client) }
+
+  context 'GET /users' do
+    let(:url) { '/admin/v1/users' }
+    let!(:users) { create_list(:user, 5) }
+    before(:each) { get url, headers: auth_header(login_user) }
+    include_examples 'forbidden access'
+  end
+
+  context 'POST /users' do
+    let(:url) { '/admin/v1/users' }
+    before(:each) { get url, headers: auth_header(login_user) }
+    include_examples 'forbidden access'
+  end
+
+  context 'PATCH /users/:id' do
+    let(:user) { create(:user) }
+    let(:url) { "/admin/v1/users/#{user.id}" }
+    before(:each) { get url, headers: auth_header(login_user) }
+    include_examples 'forbidden access'
+  end
+
+  context 'DELETE /users/:id' do
+    let!(:user) { create(:user) }
+    let(:url) { "/admin/v1/users/#{user.id}" }
+    before(:each) { get url, headers: auth_header(login_user) }
+    include_examples 'forbidden access'
+  end
+end
