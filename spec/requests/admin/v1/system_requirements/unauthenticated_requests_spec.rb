41c14ac6088f3b66bf35c15d2cdea9486014dcc2
Frankyston Lins <frankyston@gmail.com>
2020-12-27 11:23:54 -0300

    Adding CRUD of System Requirements

---
+++ spec/requests/admin/v1/system_requirements/unauthenticated_requests_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Admin::V1::SystemRequirements as :unauthenticated', type: :request do
+
+  context 'GET /system_requirements' do
+    let(:url) { '/admin/v1/system_requirements' }
+    let!(:system_requirements) { create_list(:system_requirement, 5) }
+    before(:each) { get url }
+    include_examples 'unauthenticated access'
+  end
+
+  context 'POST /system_requirements' do
+    let(:url) { '/admin/v1/system_requirements' }
+    before(:each) { get url }
+    include_examples 'unauthenticated access'
+  end
+
+  context 'PATCH /system_requirements/:id' do
+    let(:system_requirement) { create(:system_requirement) }
+    let(:url) { "/admin/v1/system_requirements/#{system_requirement.id}" }
+    before(:each) { get url }
+    include_examples 'unauthenticated access'
+  end
+
+  context 'DELETE /system_requirements/:id' do
+    let!(:system_requirement) { create(:system_requirement) }
+    let(:url) { "/admin/v1/system_requirements/#{system_requirement.id}" }
+    before(:each) { get url }
+    include_examples 'unauthenticated access'
+  end
+end
